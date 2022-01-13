b82f406cb9a4c22d2305dc16903a2a6b52057311
petini96 <petiniprojetos96@gmail.com>
2022-01-13 15:39:46 -0300

    ah seu feladaputa safado

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -1,25 +1,29 @@
 package db
+
 import (
 	"database/sql"
 	"log"
 	"os"
 	"testing"
-	_"github.com/lib/pq"
+
+	_ "github.com/lib/pq"
 )
+
 const (
 	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5435/simple_blank?sslmode=disable"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_blank?sslmode=disable"
 )
+
 var testQueries *Queries
 var testDB *sql.DB
 
-func TestMain(m *testing.M){
+func TestMain(m *testing.M) {
 	var err error
 	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db: ", err)
 	}
-	testQueries = New(testDB) 
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
